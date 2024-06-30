84dd6418d30233b39ba251a5ba0669846b9681ba
bagil01 <bagil2203@gmail.com>
2024-06-30 18:01:30 -0300

    novidades

---
+++ javascript/principal.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const ceqButton = document.querySelector(".buttons .neumorphic:nth-child(1)");
+    
+    ceqButton.addEventListener("click", () => {
+      window.location.href = "03_descrição.html"; // Substitua "pagina_ceq.html" pelo caminho da página desejada
+    });
+    
+    
+});
